f55dba66ba4d5c3873ef2413b9b865da6a05c27d
baszurburg <sebastiaan.zurburg@ing.nl>
2016-03-21 23:52:17 +0100

    nieuws navigation added

---
+++ app/pages/nieuws/nieuws.js
@@ -0,0 +1,7 @@
+var BasePage = require("../../shared/BasePage");
+
+var NieuwsPage = function() {};
+NieuwsPage.prototype = new BasePage();
+NieuwsPage.prototype.constructor = NieuwsPage;
+
+module.exports = new NieuwsPage();
